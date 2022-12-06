094d613f74fe0c1b66763fba651c6b7d8d65505c
Markus AP <89122836+markus-ap@users.noreply.github.com>
2022-12-06 08:51:06 +0100

    sjette desember 🎄

--- 2022/20221206.py
+++ 2022/20221206.py
@@ -1,7 +1,17 @@
 from _verktøy import les_fil
 
+def finn_start(data, avvik: int):
+    for i in range(len(data)):
+        datapunktar = list(data[i:i+avvik])
+        datasett = set(datapunktar)
+        if len(datasett) is len(datapunktar): 
+            print(i, datasett, datapunktar)
+            return i+avvik
+
 def hovud():
     data = les_fil("20221206.txt")
+    start = finn_start(data, 14)
+    print(start)
 
 if __name__ == "__main__":
     hovud()
